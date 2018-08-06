261a56959b7f1cb55fb1421967e23deef94d9389
Tim Chang <tc932@drexel.edu>
2018-08-06 11:34:15 -0700

    established connection to mongo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,34 @@
 import http from 'http';
 
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const fs = require('fs');
+const mongoose = require('mongoose');
+const assert = require('assert')
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json())
+
+if (! fs.existsSync('./env.sh')) {
+  throw new Error('env.sh file is missing');
+}
+if (! process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+}
+mongoose.connection.on('connected', function() {
+  console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function() {
+  console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
+  process.exit(1);
+});
+mongoose.connect(process.env.MONGODB_URI);
+
+
+
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
